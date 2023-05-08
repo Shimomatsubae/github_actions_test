74d291945c81e4895b8f7bdb47c07ad1a24e7624
shimoe <shimomatsubae@takumi-robo.com>
2023-05-08 18:11:47 +0900

    feat: add function

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import numpy as np
 
-vec1 = np.array([1, 2, 3])
-print(vec1)
+
+def sum_array(vec1, vec2):
+    vec = vec1 + vec2
+    print(vec)
+
+
+if __name__ == "__main__":
+    vec1 = np.array([1, 2, 3])
+    vec2 = np.array([4, 5, 6])
+    sum_array(vec1, vec2)
